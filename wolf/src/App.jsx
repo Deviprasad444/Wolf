84695ef8490cc32007a122c9d438b284b4cee2c4
iOTA <debi1234@gmail.com>
2026-03-08 18:23:20 +0530

    c1

---
+++ wolf/src/App.jsx
@@ -0,0 +1,17 @@
+import Dog from './components/Dog'
+import {Canvas} from '@react-three/fiber'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Canvas>
+         <Dog/>
+    </Canvas>
+  
+    </>
+  )
+}
+
+export default App
---
+++ wolf/src/components/Dog.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Canvas ,useThree} from '@react-three/fiber'
+
+const Dog =() =>{
+
+    useThree(({camera, scene, gl})=>{
+        console.log(camera.position)
+
+    })
+    return(
+    
+            <mesh>
+                <meshBasicMaterial color ={0x00FF00}/>
+                <boxGeometry args ={[1,1,1]}/>
+            </mesh>
+
+    )
+}
+export default Dog
