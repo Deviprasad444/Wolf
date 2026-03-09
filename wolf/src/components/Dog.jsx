2230edd1f50984b71436ca61808750a08000b001
iOTA <debi1234@gmail.com>
2026-03-09 23:57:29 +0530

    login

--- wolf/src/components/Dog.jsx
+++ wolf/src/components/Dog.jsx
@@ -14,6 +14,7 @@ const Dog =() =>{
 
     return(
     <>
+    <p> hello this is me</p>
     <primitive object={model.scene} position ={[0.2, -0.5, 0]} rotation ={[0, Math.PI/3, 0]} />
     <OrbitControls/>
     
