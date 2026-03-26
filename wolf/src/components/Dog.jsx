46263f73480f96b0fe167f40c49704f7450f429f
Debiprasad <debiprasadsing2003@gmail.com>
2026-03-26 23:53:52 +0530

    bracnhes added

--- wolf/src/components/Dog.jsx
+++ wolf/src/components/Dog.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import * as three from 'three'
 import {Canvas, useThree} from '@react-three/fiber'
-import {OrbitControls, useGLTF, useTexture} from '@react-three/drei'
+import {OrbitControls, useGLTF, useTexture, useAnimations} from '@react-three/drei'
 
 const Dog =() =>{
     const model= useGLTF('/models/dog.drc.glb')
     useThree(({camera, scene, gl}) =>{
-        console.log(camera.position)
+
         camera.position.z =1
+        gl.toneMapping =three.reinhardToneMapping
+        gl.outputColorSpace =three.SRGBColorSpace
         
         
     })
 
-    const textures =useTexture({
-        normalMap:"/dog_normals.jpg"
+
+    const {actions} =useAnimations(model.animations,model.scene)
+
+    useEffect(() =>{
+        actions["Take 001"].play()
+    },[actions])
+
+
+   const[normalMap, sampleMatCap, branchMap, branchNormalMap] =(useTexture(["/dog_normals.jpg", "/matcap/mat-2.png","/branches_diffuse.jpg","/branches_normals.jpg"]))
+
+    .map(texture =>{
+        texture.flipY =false
+        texture.colorSpace =three.SRGBColorSpace
+        return texture
+    })
+
+    const dogMaterial =new three.MeshMatcapMaterial({
+        normalMap:normalMap,
+        matcap:sampleMatCap
+    })
+
+    const branchMaterial =new three.MeshMatcapMaterial({
+        normalMap:branchNormalMap,
+        map:branchMap
     })
 
     model.scene.traverse((child) =>{
         if(child.name.includes("DOG")){
-            child.material =new three.MeshMatcapMaterial({
-                normalMap:textures.normalMap,
-                color:0xFF0000
-            })
+            child.material =dogMaterial
             
+        }else{
+            child.material =branchMaterial
         }
     })
 
